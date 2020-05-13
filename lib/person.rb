d53a72b67b9f9997e96885d860decd49ece649fd
johnloberger <johnloberger@gmail.com>
2020-05-13 16:14:58 -0500

    john drive finish lab

--- lib/person.rb
+++ lib/person.rb
@@ -32,7 +32,40 @@ class Person
     self.hygiene > 7
   end
 
+  def get_paid(salary)
+    self.bank_account += salary
+    "all about the benjamins"
+  end
   
+  def take_bath
+    self.hygiene += 4
+    "♪ Rub-a-dub just relaxing in the tub ♫"
+  end
 
+  def work_out
+    self.hygiene -= 3
+    self.happiness += 2
+    "♪ another one bites the dust ♫"
+  end
+
+  def call_friend(friend)
+    self.happiness += 3
+    friend.happiness += 3
+    "Hi #{friend.name}! It's #{self.name}. How are you?"
+  end
+
+  def start_conversation(person, topic)
+    if topic == "politics"
+      person.happiness -= 2
+      self.happiness -= 2
+      "blah blah partisan blah lobbyist"
+    elsif topic == "weather"
+      person.happiness += 1
+      self.happiness += 1
+      "blah blah sun blah rain"
+    else topic == "other"
+      "blah blah blah blah blah"
+    end 
+  end
 
 end
