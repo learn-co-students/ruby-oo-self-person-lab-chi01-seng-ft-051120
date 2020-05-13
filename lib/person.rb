42cd96b3fb2aad3d5f2c99f0d71c5856de2df556
DanHazard <daniel.love.jr@gmail.com>
2020-05-13 12:10:07 -0500

    Dan's driving session

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,38 @@
 # your code goes here
+
+class Person
+
+  attr_accessor :bank_account, :happiness, :hygiene
+  attr_reader :name
+
+  def initialize(name, bank_account = 25, happiness = 8, hygiene = 8)
+    @name = name
+    @bank_account = bank_account
+    @happiness = happiness
+    @hygiene = hygiene
+  end
+
+  def happiness= (num)
+    @happiness = num
+    @happiness = 10 if @happiness > 10
+    @happiness = 0 if @happiness < 0
+  end
+
+  def hygiene= (num)
+    @hygiene = num
+    @hygiene = 10 if @hygiene > 10
+    @hygiene = 0 if @hygiene < 0
+  end
+
+  def happy?
+    self.happiness > 7
+  end
+
+  def clean?
+    self.hygiene > 7
+  end
+
+  
+
+
+end
