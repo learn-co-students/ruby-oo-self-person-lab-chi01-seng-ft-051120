f0fc62f7c398f3fc7a67ebd51317b8255c8316cb
okaydokay97 <rubens.fernandes97@gmail.com>
2020-05-13 16:30:28 -0500

    finished lab

--- lib/person.rb
+++ lib/person.rb
@@ -2,32 +2,28 @@
 require 'pry'
 class Person
     attr_reader :name, :happiness, :hygiene
-    attr_accessor :balance
+    attr_accessor :bank_account
     def initialize(name)
         @name = name
-        @balance = 25
+        @bank_account = 25
         @happiness = 8
         @hygiene = 8
     end
 
     def happiness=(happiness)
-        if (happiness < 0 || happiness > 10)
-            puts "Incorrect value!"
-        else
             @happiness = happiness
-        end
+            @happiness = 10 if @happiness > 10 
+            @happiness = 0 if @happiness < 0
     end
 
     def hygiene=(hygiene)
-        if (hygiene < 0 || hygiene > 10)
-            puts "Incorrect value!"
-        else
-            @hygine = hygiene
-        end
+        @hygiene = hygiene
+        @hygiene = 10 if @hygiene > 10 
+        @hygiene = 0 if @hygiene < 0
     end
 
     def clean?
-        if self.hygine > 7
+        if self.hygiene > 7
             return true
         else
             return false
@@ -43,9 +39,39 @@ class Person
     end
 
     def get_paid(amount)
-        self.balance += amonut
+        self.bank_account += amount
         "all about the benjamins"
     end
+    
+    def take_bath
+        self.hygiene += 4
+        "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
 
+    def work_out
+        self.happiness += 2
+        self.hygiene -= 3
+        "♪ another one bites the dust ♫"
+    end
 
+    def call_friend(friend)
+        friend.happiness += 3
+        self.happiness += 3
+        "Hi #{friend.name}! It's #{self.name}. How are you?"
+    end
+
+    def start_conversation(friend, topic)
+        if topic == "politics"
+            self.happiness -= 2
+            friend.happiness -= 2
+            "blah blah partisan blah lobbyist"
+        elsif topic == "weather"
+            self.happiness += 1
+            friend.happiness += 1
+            "blah blah sun blah rain"
+        else
+            "blah blah blah blah blah"
+        end
+    end
 end
+
